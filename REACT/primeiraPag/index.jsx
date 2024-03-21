c92a988442b76ea45f5f6e50b3535e8a9d526bb4
romeolacerda <romeolacerdafarias@gmail.com>
2024-03-21 15:33:22 -0300

    .

---
+++ REACT/primeiraPag/index.jsx
@@ -0,0 +1,14 @@
+const element = <h1>Olá Mundo</h1>;
+
+const aelement = (
+  <div>
+    <h1>Texto</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+);
