4c6a815ccbde75cd7c032dd5ba08f332595d5f2e
romeolacerda <romeolacerdafarias@gmail.com>
2024-03-29 00:04:15 -0300

    .

--- REACT/reactVite/first react project/src/App.jsx
+++ REACT/reactVite/first react project/src/App.jsx
@@ -1,3 +1,18 @@
-function App() {
-  return {};
+export default function App() {
+  return (
+    <div>
+      <h1>Ola mundo</h1>
+      <p>lorem</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, eos.
+      </p>
+      <ul>
+        <li>Lorem, ipsum.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet consectetur.</li>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem ipsum dolor sit, amet consectetur adipisicing.</li>
+      </ul>
+    </div>
+  );
 }
