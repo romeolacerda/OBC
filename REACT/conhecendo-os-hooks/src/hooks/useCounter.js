db59ba99c53ee5af26627be42fa855578c6e982d
romeolacerda <romeolacerdafarias@gmail.com>
2024-04-06 19:10:58 -0300

    .

--- REACT/conhecendo-os-hooks/src/hooks/useCounter.js
+++ REACT/conhecendo-os-hooks/src/hooks/useCounter.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
 
+function getInitialValue() {
+  console.log("Obtendo o valor inicial");
+  return 1 + 1;
+}
+
 export default function useCounter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => getInitialValue());
 
   const increment = () => {
-    setCount(count + 1);
+    setCount((currentState) => currentState + 1);
+    setCount((currentState) => currentState + 1);
   };
 
   return { count, increment };
