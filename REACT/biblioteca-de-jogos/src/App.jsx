03de8bd6c0cf3a9302cdffb1f6c3502b73859b00
romeolacerda <romeolacerdafarias@gmail.com>
2024-04-07 17:30:07 -0300

    .

---
+++ REACT/biblioteca-de-jogos/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Biblioteca de Jogos</h1>
+      <form>
+        <div>
+          <label htmlFor="title">titulo:</label>
+          <input type="text" />
+        </div>
+      </form>
+    </div>
+  );
+}
