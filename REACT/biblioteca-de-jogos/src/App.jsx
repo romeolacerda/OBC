9054c819c9a23c8f135317d8920f762704672b60
romeolacerda <romeolacerdafarias@gmail.com>
2024-04-08 11:17:54 -0300

    .

--- REACT/biblioteca-de-jogos/src/App.jsx
+++ REACT/biblioteca-de-jogos/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Game from "./components/Game";
 
 export default function App() {
   const [games, setGames] = useState(() => {
@@ -62,13 +63,12 @@ export default function App() {
       </form>
       <div className="games">
         {games.map((game) => (
-          <div key={game.id}>
-            <img src={game.cover} alt="" />
-            <div>
-              <h2>{game.title}</h2>
-              <button onClick={() => removeGame(game.id)}>Remover</button>
-            </div>
-          </div>
+          <Game
+            key={game.id}
+            title={game.title}
+            cover={game.cover}
+            onRemove={() => removeGame(game.id)}
+          />
         ))}
       </div>
     </div>
