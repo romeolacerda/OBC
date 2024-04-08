afcbe52db170ba1a46cd9a5c2a374860b1e14d22
romeolacerda <romeolacerdafarias@gmail.com>
2024-04-08 11:56:35 -0300

    .

--- REACT/biblioteca-de-jogos/src/App.jsx
+++ REACT/biblioteca-de-jogos/src/App.jsx
@@ -1,32 +1,9 @@
-import { useState } from "react";
 import Game from "./components/Game";
 import NewGameForm from "./components/NewGameForm";
+import useGameCollection from "./hooks/useGameCollection";
 
 export default function App() {
-  const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obc-game-lib");
-    if (!storedGames) return [];
-    const gameArray = JSON.parse(storedGames);
-    return gameArray;
-  });
-
-  const addGame = ({ title, cover }) => {
-    const id = Math.floor(Math.random() * 1000000);
-    const game = { id, title, cover };
-    setGames((state) => {
-      const newState = [...state, game];
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState));
-      return newState;
-    });
-  };
-
-  const removeGame = (id) => {
-    setGames((state) => {
-      const newState = state.filter((game) => game.id !== id);
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState));
-      return newState;
-    });
-  };
+  const { games, addGame, removeGame } = useGameCollection();
 
   return (
     <div className="app">
