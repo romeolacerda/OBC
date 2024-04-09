c18b29d4a348e807794d5dc18242dde73db258ef
romeolacerda <romeolacerdafarias@gmail.com>
2024-04-09 17:18:14 -0300

    .

--- REACT/conhecendo-useeffect/src/App.jsx
+++ REACT/conhecendo-useeffect/src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 async function fetchPokemon() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon");
   const data = await response.json();
@@ -5,9 +7,27 @@ async function fetchPokemon() {
 }
 
 export default function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetchPokemon().then((results) => {
+      console.log("Requisição atualizada.");
+      console.log(results);
+      setPokemon(results);
+    });
+  }, []);
+
   return (
     <div className="app">
       <div>Pokemon</div>
+      <ul className="pokemon">
+        {pokemon.map((mon) => (
+          <li key={mon.name}>
+            <span>{mon.name}</span>
+            <button>Ver Detalhes</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
